14e5d1e8cde691e70cff7689949bce157b5f51bd
nmro184 <nimrod.balaban@gmail.com>
2024-09-13 10:47:51 +0300

    revised

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-user_input = input("write your crontab expression - (seperate each field with a space\n")
+user_input = input("write your crontab expression - (seperate each field with a space\nnote - use either step - '/x' range - 'x-y' or a list - x,y,z\n" )
+ALLOWED_MINUTES_RANGE = range(0,59)
 def crontab_test(expression):
     flag = True
     parts = expression.split()
@@ -8,28 +9,49 @@ def crontab_test(expression):
     print("valid" if flag else "not valid")
 
 def valid_minutes(input):
-    if len(input) <1 :
+    if len(input) <1 : #checking for * or a single number
         return False
+    if input == '*':
+        return True
     if len(input) == 1:
-        if input == '*':
-            return True
-        if int(input) in range(0,59):
-            return True    
+        if int(input) in ALLOWED_MINUTES_RANGE:
+            return True   
+        else:
+            return False 
     else:
-        return False
-    if len(input) > 1:
-        if len(input) == 2:
-            return False
-        if len(input) == 3:
-            if '-' in input:
-                index = input.find('-')
-                if((int(input[index - 1]) in range(0,59)) and (int(input[index + 1]) in range(0,59)) and (int(input[index - 1]) < int(input[index + 1]))):
-                    return True
+        if len(input) > 1:
+            if len(input) == 2:#checking for step
+                if '/' in input:
+                    if input[1] in ALLOWED_MINUTES_RANGE:
+                        return True
+                    else:
+                        return False
                 else:
                     return False
-            else:
-                if ',' in input:
-                    return True #fix needed
+            if len(input) == 3: #checking for a range/short list
+                if '-' in input:
+                    index = input.find('-')
+                    if((input[0] in ALLOWED_MINUTES_RANGE) and (input[3] in ALLOWED_MINUTES_RANGE) and (input[0] < input[3])):
+                        return True
+                    else:
+                        return False
+                else:
+                    if ',' in input:
+                        if input[0] in ALLOWED_MINUTES_RANGE and input[3] in ALLOWED_MINUTES_RANGE:
+                            return True
+                        else:
+                            return False
+                    else:
+                        return False      
+            else: #checking for a long list
+                i=0
+                while i < len(input):
+                    if i%2 == 0:
+                        if int(input[i] not in ALLOWED_MINUTES_RANGE):
+                            return False
+                    else:
+                        if input[i] != ',':
+                            return False
 
 
 
