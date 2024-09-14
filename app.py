9a67a160700b0e32d4fe7d8fecc639316f02a96c
nmro184 <nimrod.balaban@gmail.com>
2024-09-14 15:15:54 +0300

    revised

--- app.py
+++ app.py
@@ -1,57 +1,82 @@
 
-user_input = input("write your crontab expression - (seperate each field with a space\nnote - use either step - '/x' range - 'x-y' or a list - x,y,z\n" )
-ALLOWED_MINUTES_RANGE = range(0,59)
+ALLOWED_MINUTES_RANGE = range(0,60)
+ALLOWED_HOURS_RANGE = range(0,24)
+ALLOWED_DAY_OF_MONTH_RANGE = range(1,32)
+ALLOWED_MONTH_RANGE = range(1,13)
+ALLOWED_DAY_OF_WEEK_RANGE = range(0,7)
+
 def crontab_test(expression):
-    flag = True
-    parts = expression.split()
+    parts = str(expression).split()
     if len(parts) != 5:
-        flag = False
-    print("valid" if flag else "not valid")
+        return False
+    result = (
+        valid_minutes(parts[0]) and
+        valid_hours(parts[1]) and
+        valid_day_of_month(parts[2]) and
+        valid_month(parts[3]) and
+        valid_day_of_week(parts[4])
+    )
+    print(result)
+    return result
+
+def astrix_or_digit_test(input , range):
+    if '*' in input:
+        if input == '*':
+            return True
+        else:
+            return False
+        
+    if input.isdigit():
+        if int(input) in range:
+            return True
+        else:
+            return False
+    return False
+
+def step_test(input , range):
+    if '/' in input:
+        if input.startswith('/') and (int(input[1:]) in range):
+            return True
+        else:
+            return False
+    return False
+
+def range_test(input,range):
+    if '-' in input:
+        parts = input.split('-')
+        if((int(parts[0]) in range) and (int(parts[1]) in range) and (int(parts[0]) < int(parts[1]))):
+            return True
+        else:
+            return False
+    return False
+
+def list_test(input,range):
+    if ',' in input:
+        parts = input.split(',')
+        for part in parts:
+            if int(part) not in range:
+                return False
+        return True
+    return False
+
+def valid_input(input , range):
+    return astrix_or_digit_test(input ,range) or step_test(input ,range) or range_test(input ,range) or list_test(input ,range)
 
 def valid_minutes(input):
-    if len(input) <1 : #checking for * or a single number
-        return False
-    if input == '*':
-        return True
-    if len(input) == 1:
-        if int(input) in ALLOWED_MINUTES_RANGE:
-            return True   
-        else:
-            return False 
-    else:
-        if len(input) > 1:
-            if len(input) == 2:#checking for step
-                if '/' in input:
-                    if input[1] in ALLOWED_MINUTES_RANGE:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            if len(input) == 3: #checking for a range/short list
-                if '-' in input:
-                    index = input.find('-')
-                    if((input[0] in ALLOWED_MINUTES_RANGE) and (input[3] in ALLOWED_MINUTES_RANGE) and (input[0] < input[3])):
-                        return True
-                    else:
-                        return False
-                else:
-                    if ',' in input:
-                        if input[0] in ALLOWED_MINUTES_RANGE and input[3] in ALLOWED_MINUTES_RANGE:
-                            return True
-                        else:
-                            return False
-                    else:
-                        return False      
-            else: #checking for a long list
-                i=0
-                while i < len(input):
-                    if i%2 == 0:
-                        if int(input[i] not in ALLOWED_MINUTES_RANGE):
-                            return False
-                    else:
-                        if input[i] != ',':
-                            return False
+    return valid_input(input , ALLOWED_MINUTES_RANGE)
+
+def valid_hours(input):
+    return valid_input(input , ALLOWED_HOURS_RANGE)
+
+def valid_day_of_month(input):
+    return valid_input(input , ALLOWED_DAY_OF_MONTH_RANGE)
+
+def valid_month(input):
+    return valid_input(input , ALLOWED_MONTH_RANGE)
+
+def valid_day_of_week(input):
+    return valid_input(input , ALLOWED_DAY_OF_WEEK_RANGE)
 
 
-
+user_input = input("write your crontab expression - (seperate each field with a space)\nnote - use either step - '/x' range - 'x-y' or a list - x,y,z\n" )
+crontab_test(user_input)
